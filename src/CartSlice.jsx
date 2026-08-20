bbc5025e5579a89addcb22ba4d6ed4134a79b661
bm-prime <bm-prime112@pm.me>
2026-08-20 15:19:30 +0300

    added the reducers in cartslice to control store

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,20 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const existingItem = state.items.find(item => item.name === action.payload.name);
+      if (!existingItem) {
+        state.items.push({ ...action.payload, quantity: 1 }); // Add the new item with quantity 1
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload.name);
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      const ItemToUpdate = state.items.find(item => item.name === name);
+      if (ItemToUpdate) {
+        ItemToUpdate.quantity = quantity;
+      }
     },
   },
 });
